94e44fbdd8fd8db944d2104d316bc213f68cfa7a
Your Name <you@example.com>
2025-08-29 11:03:15 +0200

    Live Search added

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+let allPokemon = [];
+
 const content = document.getElementById("content");
+const searchInput = document.getElementById("searchInput");
 
 let currentOffset = 0;
 
+document.getElementById("searchInput").addEventListener("input", filterPokemon);
+document.getElementById("searchBtn").addEventListener("click", searchPokemon);
 document.getElementById("loadMoreBtn").addEventListener("click", () => {
   currentOffset += 20;
   init();
@@ -17,16 +22,26 @@ async function renderPokemon(limit, offset) {
 
 async function init() {
   content.innerHTML = "";
-  const pokemonData = await renderPokemon(20, currentOffset);
-  for (const pokemon of pokemonData.results) {
+  allPokemon = await renderPokemon(151, 0);
+  displayPokemon(allPokemon.results.slice(0, 20));
+}
+init();
+
+async function fetchPokemon(searchTerm) {
+  const search = `https://pokeapi.co/api/v2/pokemon/${searchTerm}`;
+  const resp = await fetch(search);
+  const searchPkm = await resp.json();
+
+  return searchPkm;
+}
+
+async function displayPokemon(pokemonList) {
+  for (const pokemon of pokemonList) {
     const pokemonDetails = await getPokemonDetails(pokemon.url);
     const card = createPokemonCard(pokemon, pokemonDetails);
-    console.log(pokemonDetails);
-
     content.appendChild(card);
   }
 }
-init();
 
 function createPokemonCard(pokemonData, pokemonDetails) {
   const card = document.createElement("div");
@@ -51,4 +66,28 @@ async function getPokemonDetails(url) {
   const data = await response.json();
 
   return data;
+}
+
+async function searchPokemon() {
+  try {
+    content.innerHTML = "";
+    const searchTerm = searchInput.value.trim();
+    const pokemonsearch = await fetchPokemon(searchTerm);
+
+    const card = createPokemonCard(pokemonsearch, pokemonsearch);
+    content.appendChild(card);
+  } catch {
+    document.getElementById(
+      "content"
+    ).innerHTML = `<h2>Pokemon nicht gefunden</h2>`;
+  }
+}
+
+function filterPokemon() {
+  content.innerHTML = "";
+  const searchTerm = searchInput.value.toLowerCase();
+  const filteredPokemon = allPokemon.results.filter((pokemon) => {
+    return pokemon.name.toLowerCase().includes(searchTerm);
+  });
+  displayPokemon(filteredPokemon);
 }
